2e85fc904c81a72ed1bd3cf493a5fda96ce2e9d6
Nico Rath <nico.rath@rathsolutions.de>
2021-08-10 00:35:09 +0200

    added license header

--- src/main/java/de/rathsolutions/jpa/entity/Project.java
+++ src/main/java/de/rathsolutions/jpa/entity/Project.java
@@ -1,9 +1,34 @@
+/*-
+ * #%L
+ * SchoolfinderBackend
+ * %%
+ * Copyright (C) 2020 - 2021 Rathsolutions. <info@rathsolutions.de>
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
 package de.rathsolutions.jpa.entity;
 
 import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.OneToMany;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +37,8 @@ import lombok.Setter;
 @Setter
 public class Project {
 
+    @Id
+    @GeneratedValue
     private long id;
 
     private String projectName;
